4cff46678e992b996ad16a7c652179cd33fe8a14
parha <parha@DESKTOP-GIVD1R1>
2024-02-15 16:14:14 +0100

    Add project files.

---
+++ Models/Boss.cs
@@ -0,0 +1,18 @@
+﻿namespace FirstBlazorApp.Models
+{
+    public class Boss
+    {
+        public string Firstname;
+        public string Lastname;
+        public string Department; 
+        public double Salary;
+
+        public Boss(string firstname, string lastname, string department, double salary)
+        {
+            Firstname = firstname;
+            Lastname = lastname;
+            Department = department;
+            Salary = salary;
+        }
+    }
+}
---
+++ Models/Worker.cs
@@ -0,0 +1,18 @@
+﻿namespace FirstBlazorApp.Models
+{
+    public class Worker
+    {
+        public string Firstname;
+        public string Lastname;
+        public string Boss;
+        public double Wage;
+
+        public Worker(string firstname, string lastname, string boss, double wage)
+        {
+            Firstname = firstname;
+            Lastname = lastname;
+            Boss = boss;
+            Wage = wage;
+        }
+    }
+}
