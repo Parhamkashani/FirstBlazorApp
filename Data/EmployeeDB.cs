fc48ad911fbd55acef83f3f1a2bf7723a1d83435
Damien Weatherley <ritualofchud@gmail.com>
2024-02-24 17:07:58 +0100

    Layout - Changed the folder layout of the project - Updated the CSS for EmployeeDetail

--- Data/EmployeeDB.cs
+++ Data/EmployeeDB.cs
@@ -5,21 +5,19 @@ namespace FirstBlazorApp.Data
 {
     public class EmployeeDB
     {
-        private static List<Boss> _bosses = new List<Boss>()
+        private List<Boss> _bosses = new List<Boss>()
         { 
             new Boss(1, "Damien", "Weatherley", "Programming", 100000, 30, "Webdesigning"),
             new Boss(2, "Parham", "Kashani", "CEO", 1000000, 27, "Management"),
         };
 
-        private static List<Worker> _workers = new List<Worker>()
+        private List<Worker> _workers = new List<Worker>()
         {
             new Worker(3, "Alex", "England", "Damien Weatherley", 28.5, 36, "Accountant"),
             new Worker(4, "Bob", "Scotland", "Damien Weatherley", 20, 25, "Selling"),
             new Worker(5, "Sally", "Wales", "Damien Weatherley", 25, 22, "Handling"),
             new Worker(6, "Tony", "Ireland", "Damien Weatherley", 43, 29, "Comment"),
         };
-
-        public List<Employee> _employees = new List<Employee>();
 
         public List<Worker> GetWorkers()
         {
@@ -33,18 +31,14 @@ namespace FirstBlazorApp.Data
 
         public List<Employee> GetEmployees()
         {
-            foreach(Worker worker in _workers)
-            {
-                _employees.Add(worker);
-            }
-            
-            foreach(Boss boss in _bosses)
-            {
-                _employees.Add(boss);
-            }
+            var list = new List<Employee>();
 
-            return _employees;
+            _workers.ForEach(w => list.Add(w));
+            _bosses.ForEach(b => list.Add(b));
+
+            return list;
         }
+
         public Boss GetBossByID(int id)
         {
             Boss b = _bosses.Find(x => x.Id == id) ?? new Boss();
@@ -62,7 +56,7 @@ namespace FirstBlazorApp.Data
         public Employee GetEmployeeByID(int id)
         {
             List<Employee> _employee = GetEmployees();
-            Employee e = _employee.Find(x => x.Id == id);
+            Employee e = _employee.Find(x => x.Id == id) ?? new Employee(); ;
 
             return e;
         }
